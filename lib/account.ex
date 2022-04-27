3309640e74370a2793b8b9ae5c09e0409ee91e74
Papa Attrams <48452451+Attrams@users.noreply.github.com>
2022-04-27 21:05:45 +0000

    add the withdraw/2

--- lib/account.ex
+++ lib/account.ex
@@ -9,22 +9,39 @@ defmodule Account do
     GenServer.start_link(__MODULE__, :ok)
   end
 
+  # API
   def get_balance(pid) do
     GenServer.call(pid, :get_balance)
-  end
-
-  def handle_call(:get_balance, _from, state) do
-    {:reply, Map.get(state, :balance), state}
   end
 
   def deposit(pid, amount) do
     GenServer.cast(pid, {:deposit, amount})
   end
 
-  def handle_cast({:deposit, amount}, state) do
-    {_value, balance_with_deposit} =
-      Map.get_and_update(state, :balance, fn balance -> {balance, balance + amount} end)
+  def withdraw(pid, amount) do
+    GenServer.cast(pid, {:withdraw, amount})
+  end
 
-    {:noreply, balance_with_deposit}
+  # Callbacks
+  def handle_call(:get_balance, _from, state) do
+    {:reply, Map.get(state, :balance), state}
+  end
+
+  def handle_cast({:deposit, amount}, state) do
+    {:noreply, get_update_balance(state, amount, &(&1 + &2))}
+  end
+
+  def handle_cast({:withdraw, amount}, state) do
+    {:noreply, get_update_balance(state, amount, &(&1 - &2))}
+  end
+
+  # Private
+  defp get_update_balance(state, amount, calculate_balance) do
+    {_value, updated_balance} =
+      Map.get_and_update(state, :balance, fn balance ->
+        {balance, calculate_balance.(balance, amount)}
+      end)
+
+    updated_balance
   end
 end
